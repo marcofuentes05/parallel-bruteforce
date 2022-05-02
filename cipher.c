8d604db6fdb9846cc70908eabae165f29b74f854
mfuentes05 <marcofuentes1607@gmail.com>
2022-05-01 22:41:11 -0600

    Cipher: File to read a directory, encrypt it and write it onto another onte

--- cipher.c
+++ cipher.c
@@ -38,11 +38,19 @@ int tryKey(long key, char *ciph, int len, char* search){
 
 
 int main(int argc, char * argv[]) {
-  char *plain = "testt";
-  char *needle = "tt";
-  int len = 5; 
+  char *needle = "Lorem";
+
+  FILE *fp;
+   char plain[255];
+   fp = fopen("files/lorem.txt", "r");
+  fgets(plain, 255, (FILE*)fp);
+
+  printf("FILE CONTENT IS: %s\n", plain);
+
+  int len = strlen(plain); 
+  printf("len: %d\n", len);
   char temp[len+1];
-  long key = 555;
+  long key = 11;
   memcpy(temp, plain, len);
   temp[len]=0;
   printf("%s\n", temp);
@@ -52,6 +60,13 @@ int main(int argc, char * argv[]) {
   // printf("%s\n", temp);
   // char *ret = strstr((char *)temp, needle);
   // printf("Occurrence is: %s\n", ret);
+
+  FILE *outFp;
+  outFp = fopen("files/ciphered.txt", "w+");
+   // fprintf(outFp, "This is testing for fprintf...\n");
+   fputs(temp, outFp);
+   fclose(outFp);
+
   if (tryKey(key, temp, len, needle)) {
     printf("CIPHER WORKS!\n");
   }
